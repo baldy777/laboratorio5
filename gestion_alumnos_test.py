c7b4143aa58a38cb3e2b0993a400b5b46ffef011
baldy777 <jhoelquispetapia315@gmail.com>
2025-03-12 09:58:02 -0400

    codigo comentado por blady

--- gestion_alumnos_test.py
+++ gestion_alumnos_test.py
@@ -1,13 +1,18 @@
+'''importar libreria para busqueda de errores'''
 import unittest
 
+'''metodo para buscar alumnos si existe en el archivo'''
 def buscar_alumno(alumnos, nombre):
     for alumno in alumnos:
         if alumno["nombre"] == nombre:
             return alumno
     return None
 
+'''clase para mandar resultados fuera del archivo 
+    en caso de que este exista'''
 class TestGestionAlumnos(unittest.TestCase):
 
+    '''metodo de asignacion de datos'''
     def setUp(self):
         self.alumnos = [
             {"nombre": "Ana", "edad": 20, "carrera": "Ingeniería"},
@@ -15,6 +20,7 @@ class TestGestionAlumnos(unittest.TestCase):
             {"nombre": "María", "edad": 21, "carrera": "Física"}
         ]
 
+    ''' funciones para ver si axiste alumno buscado'''
     def test_buscar_alumno_existente(self):
         resultado = buscar_alumno(self.alumnos, "Ana")
         self.assertIsNotNone(resultado)
@@ -24,5 +30,6 @@ class TestGestionAlumnos(unittest.TestCase):
         resultado = buscar_alumno(self.alumnos, "Carlos")
         self.assertIsNone(resultado)
 
+'''inicializacion de metodo unittest'''
 if __name__ == "__main__":
     unittest.main()
